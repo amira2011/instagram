72a117ab4377d880fc5dc79484005c0450ed985f
abid <shaikh.abidali@gmail.com>
2019-06-28 22:03:46 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'pages/home'
   devise_for :users
+  devise_scope :user do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
   get 'home/index'
   root to: 'pages#home'
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
